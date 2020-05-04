806b8b2001ee54bf8e3910d20169461c589ea3f9
Eric F <nctilucent@gmail.com>
2020-05-04 21:46:55 +0300

    Updates

---
+++ precache-manifest.91bab22227cc9a07dc2248ee247e41e3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ac153059d666f843228fcbbcdcc0b6",
+    "url": "/polls/index.html"
+  },
+  {
+    "revision": "2ce8a19f4358f87c1c91",
+    "url": "/polls/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "48e181a661a9b8720818",
+    "url": "/polls/static/css/main.68bc3c38.chunk.css"
+  },
+  {
+    "revision": "2ce8a19f4358f87c1c91",
+    "url": "/polls/static/js/2.8d79308a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/polls/static/js/2.8d79308a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e181a661a9b8720818",
+    "url": "/polls/static/js/main.7176536a.chunk.js"
+  },
+  {
+    "revision": "342db7f74ca1879dffc6",
+    "url": "/polls/static/js/runtime-main.0debbc82.js"
+  },
+  {
+    "revision": "9d2b3b49fdbaf67b82b3bdfb098479a7",
+    "url": "/polls/static/media/FuturaPT-Bold.9d2b3b49.woff"
+  },
+  {
+    "revision": "c431096955795da262fc32297d7bfa8f",
+    "url": "/polls/static/media/FuturaPT-Book.c4310969.woff"
+  },
+  {
+    "revision": "65ee0094094ad87a468482025e755626",
+    "url": "/polls/static/media/FuturaPT-Demi.65ee0094.woff"
+  },
+  {
+    "revision": "92aa0d7878fe4d8d55dab98aae0796e5",
+    "url": "/polls/static/media/FuturaPT-Light.92aa0d78.woff"
+  },
+  {
+    "revision": "995c64e152e8dc077222766889194951",
+    "url": "/polls/static/media/FuturaPT-Medium.995c64e1.woff"
+  },
+  {
+    "revision": "805c606fe8b9a112b8d96e0bdcee04ef",
+    "url": "/polls/static/media/empty.805c606f.png"
+  }
+]);
